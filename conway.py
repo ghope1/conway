5e57506d0c4cbddc4625c3fa5ee84ccdce7da4b2
Gavin Hope <ghope1@umbc.edu>
2023-04-22 23:23:28 -0400

    added comments, changed to start with random pixels alive and dead

--- conway.py
+++ conway.py
@@ -1,11 +1,13 @@
 import pygame
 import random
 import copy
-SCREEN_HEIGHT = 100;
-SCREEN_WIDTH = 100;
-SCALE = 4;
-UPDATES_PER_SEC = 3;
-FPS = 60;
+
+SCREEN_HEIGHT = 100 # game world height
+SCREEN_WIDTH = 100 # game world width
+SCALE = 4 # scale graphics of game world up by some amount
+UPDATES_PER_SEC = 3 # number of times to update game world per second
+FPS = 60 
+
 # pygame setup process
 pygame.init()
 screenDisplay = pygame.display.set_mode((SCREEN_WIDTH*SCALE, SCREEN_HEIGHT*SCALE))
@@ -13,56 +15,65 @@ pygame.display.set_caption('conways game of life')
 clock = pygame.time.Clock()
 currRunning = True
 
+# individual 'living' pixel
 class pixel:
-    def __init__(self, aliveStatus, age):
-        self.aliveStatus= aliveStatus
+    def __init__(self, isAlive, age):
+        self.isAlive= isAlive
         self.age = age
     
     def invertStatus(self):
-        self.aliveStatus = not self.aliveStatus;
+        self.isAlive = not self.isAlive;
 
 
+# returns the board next frame corresponding with the current board
 def updateBoard(currBoard):
-    #nextBoard = [pix for pix in currBoard]
     nextBoard = copy.deepcopy(currBoard)
     for i in range(len(currBoard)):
         currPixel = currBoard[i]
         currPixelX = i % SCREEN_WIDTH
         currPixelY = i // SCREEN_HEIGHT
-        currPixelStatus = pixelSt(currPixelX, currPixelY, currBoard)
-        if currPixel.aliveStatus and currPixelStatus:
+        currPixelStatus = pixelNextFrame(currPixelX, currPixelY, currBoard)
+        if currPixel.isAlive and currPixelStatus:
             nextBoard[i].age += 1
-        elif currPixel.aliveStatus and (not currPixelStatus):
+        elif currPixel.isAlive and (not currPixelStatus):
             nextBoard[i].age == 0
-        nextBoard[i].aliveStatus = currPixelStatus
+        nextBoard[i].isAlive = currPixelStatus
 
     return nextBoard
 
-# Any live cell with fewer than two live neighbours dies, as if by underpopulation.
-# Any live cell with two or three live neighbours lives on to the next generation.
-# Any live cell with more than three live neighbours dies, as if by overpopulation.
-# Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
 
-def pixelSt(x, y, currBoard):
+# returns the status of current pixel at position x,y for the next frame 
+def pixelNextFrame(x, y, currBoard):
     currPixel = currBoard[x + y*SCREEN_HEIGHT]
-    aliveNextFrame = currPixel.aliveStatus
+    aliveNextFrame = currPixel.isAlive
     numNeighbors = 0
+
+    # checks all possible combinations x+-1, y+-1 around the current pixel
     for i in range(-1, 2):
         for j in range(-1, 2):
+            #checks for any extremes
             if abs(i) + abs(j) != 0 and 0 <= x+i and x+i < SCREEN_WIDTH and 0 <= y+j and y+j < SCREEN_HEIGHT:
-                numNeighbors += int(currBoard[x+i + (y+j)*SCREEN_HEIGHT].aliveStatus)
+                numNeighbors += int(currBoard[x+i + (y+j)*SCREEN_HEIGHT].isAlive)
 
-    if currPixel.aliveStatus and numNeighbors < 2:
+    # Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+    if currPixel.isAlive and numNeighbors < 2:
         aliveNextFrame = False;
-    elif currPixel.aliveStatus and (numNeighbors == 2 or numNeighbors == 3):
+
+    # Any live cell with two or three live neighbours lives on to the next generation.
+    elif currPixel.isAlive and (numNeighbors == 2 or numNeighbors == 3):
         aliveNextFrame = True;
-    elif currPixel.aliveStatus and numNeighbors > 3:
+    
+    # Any live cell with more than three live neighbours dies, as if by overpopulation.
+    elif currPixel.isAlive and numNeighbors > 3:
         aliveNextFrame = False;
-    elif (not currPixel.aliveStatus) and numNeighbors == 3:
+
+    # Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction
+    elif (not currPixel.isAlive) and numNeighbors == 3:
         aliveNextFrame = True;
         
     return aliveNextFrame
 
+# returns the color to make a pixel corresponding with the pixel age
 def ageToColor(age):
     if age == 0:
         return (255, 0 , 0)
@@ -78,37 +89,47 @@ def ageToColor(age):
         return (75, 0, 130)
     return (148, 0, 211)
 
-pixelStatus = [pixel(False, 0) for _ in range(SCREEN_WIDTH*SCREEN_HEIGHT)]
+
+# stores the current status of every pixel
+pixels = [pixel(random.randint(0,1), 0) for _ in range(SCREEN_WIDTH*SCREEN_HEIGHT)]
 
 paused = False
-frameCounter = 0
+
+frameCounter = 0 # counts to determine when to update screen
+
+# main game loop
 while currRunning:
-    clock.tick(FPS)
+    clock.tick(FPS) # sets game fps
+
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT: # user quits
             currRunning = False
-        if event.type == pygame.KEYDOWN:
+
+        if event.type == pygame.KEYDOWN: # users presses 'p', AKA pauses
             if event.key == pygame.K_p:
                 paused = not paused;
-        if event.type == pygame.MOUSEBUTTONUP:
-            clickPos = pygame.mouse.get_pos()
-            print(clickPos)
-            clickedIndex = (clickPos[0]//4)+SCREEN_HEIGHT*(clickPos[1]//4)
-            pixelStatus[clickedIndex].aliveStatus = True 
+
+        if event.type == pygame.MOUSEBUTTONUP: # user clicks somewhere
+            clickPos = pygame.mouse.get_pos() # gets current mouse position
+            clickedIndex = (clickPos[0]//4)+SCREEN_HEIGHT*(clickPos[1]//4) 
+            pixels[clickedIndex].isAlive = True # sets clicked pixel to be alive
+
+            # lines below draw a white rectangle to indicate that something happened and updated display
             pygame.draw.rect(screenDisplay, (255, 255, 255), pygame.Rect((clickPos[0]//4)*SCALE, (clickPos[1]//4)*SCALE, SCALE, SCALE))
             pygame.display.update()
 
+
     if not paused and (frameCounter*UPDATES_PER_SEC)%FPS == 0:
-        screenDisplay.fill((0, 0, 0))
+        screenDisplay.fill((0, 0, 0)) # erases screen
         
-        for pixelNum in range(len(pixelStatus)):
-            currPixel = pixelStatus[pixelNum]
+        for pixelNum in range(len(pixels)): 
+            currPixel = pixels[pixelNum]
             currPixelX = pixelNum % SCREEN_WIDTH
             currPixelY = pixelNum // SCREEN_HEIGHT
 
-            if currPixel.aliveStatus:
+            if currPixel.isAlive: 
                 pygame.draw.rect(screenDisplay, ageToColor(currPixel.age), pygame.Rect(currPixelX*SCALE, currPixelY*SCALE, SCALE, SCALE))
-        pixelStatus = updateBoard(pixelStatus)
+        pixels = updateBoard(pixels)
     frameCounter = (frameCounter + 1) % FPS
     pygame.display.update()
 pygame.quit()
